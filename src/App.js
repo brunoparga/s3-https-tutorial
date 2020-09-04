571eb67588f39a9864837c71213506a8271f11fc
Bruno Parga <brunoparga@gmail.com>
2020-09-04 20:24:30 +0300

    Add text to test CI/CD pipeline

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <small>If you can read this, you don't need glasses.</small>
         </p>
         <a
           className="App-link"
